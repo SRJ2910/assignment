b4da3f1f55765b23273045b65746b522c6d51138
SRJ <Suraj@SRJ>
2022-01-07 21:25:43 +0530

    bottom navigator added succesfully

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, file_names
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,8 +13,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  late TabController _tabController;
-
+  int i = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -113,7 +114,7 @@ class _HomepageState extends State<Homepage> {
                               ),
                             ),
                             SizedBox(
-                              height: 8.0,
+                              height: 10.0,
                             ),
                             Text(
                               '16°C · NewYork',
@@ -133,6 +134,129 @@ class _HomepageState extends State<Homepage> {
           ]),
         ),
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 20.0),
+        child: Positioned(
+          top: 32,
+          left: 0,
+          right: 0,
+          child: blurBox(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(45),
+              bottomLeft: Radius.circular(45),
+            ),
+            child: Container(
+              height: 66,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.1),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(45),
+                  bottomLeft: Radius.circular(45),
+                ),
+                border: Border.all(
+                  color: Colors.grey.shade900,
+                  width: 1,
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            i = 0;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.home_outlined,
+                          size: 25,
+                          color: i == 0 ? Colors.amber : Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            i = 1;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.menu_sharp,
+                          size: 25,
+                          color: i == 1 ? Colors.amber : Colors.white,
+                        )),
+                    Stack(
+                      children: <Widget>[
+                        IconButton(
+                            icon: Icon(
+                              Icons.notifications_none_rounded,
+                              size: 25,
+                              color: i == 2 ? Colors.amber : Colors.white,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                i = 2;
+                              });
+                            }),
+                        Positioned(
+                          left: 15,
+                          top: 15,
+                          child: Container(
+                            padding: const EdgeInsets.all(1),
+                            height: 8,
+                            width: 8,
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Text(
+                              '2',
+                              style: TextStyle(
+                                // color: kBlackColor,
+                                fontSize: 5,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            i = 3;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.settings_outlined,
+                          size: 25,
+                          color: i == 3 ? Colors.amber : Colors.white,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
+
+  Widget blurBox({
+    @required Widget? child,
+    BorderRadius? borderRadius,
+    double sigmaX = 5,
+    double sigmaY = 5,
+  }) =>
+      ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.zero,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: sigmaX, sigmaY: sigmaY),
+          child: child,
+        ),
+      );
 }
